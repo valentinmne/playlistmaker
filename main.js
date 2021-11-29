6b77341c14a1da0ef9721be94c8a8e4acc8f2fcc
valentinmne <valentin.moine01@gmail.com>
2021-11-29 17:39:22 +0100

    add:spotify_links_detection

---
+++ main.js
@@ -0,0 +1,28 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const client = new DiscordJS.Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES
+    ],
+  })
+
+
+    client.on('ready', () => {
+        console.log('The bot is ready!')
+      })
+      client.on('messageCreate', (msg) => {
+        if (msg.content.startsWith('https://open.spotify.com/track/')) {
+
+          msg.reply({
+            content: 'adding to playlist',
+          })
+        }
+      })
+  
+
+    client.login(process.env.TOKEN)
+
